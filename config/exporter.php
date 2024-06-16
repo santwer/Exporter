6d3989c02a5184aa1a9e98058bddc84e3660633c
antwertinger <sven.antwertinger@tricept.de>
2024-06-16 18:52:27 +0200

    set relative to false

--- config/exporter.php
+++ config/exporter.php
@@ -4,7 +4,7 @@ return [
 	'removeRelations'       => true,
 	'relationsFromTemplate' => false,
 	'temp_folder'			=> env('EXPORTER_TEMP_FOLDER', sys_get_temp_dir()),
-	'temp_folder_relative'   => env('EXPORTER_TEMP_FOLDER_RELATIVE', true),
+	'temp_folder_relative'   => env('EXPORTER_TEMP_FOLDER_RELATIVE', false),
 	'word2pdf'              => [
 		'soffice_prefix' => env('SOFFICE_PATH', ''),
 		'command'        => env('SOFFICE_COMMAND_PATH', 'soffice --convert-to pdf --outdir ? ? --headless'),
